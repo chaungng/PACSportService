2654c54fecf92d1f4cc30474cf0c224b09729b1d
Chau Ng <ngocchau0710@hotmail.com>
2020-02-15 12:29:18 -0800

    initialize PACSportService - Spring Boot

---
+++ src/main/java/com/pacsport/service/PacSportServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pacsport.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PacSportServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PacSportServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pacsport/service/PacSportServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pacsport.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PacSportServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
